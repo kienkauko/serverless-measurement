da3ad1290e3591ed7fdca436bfe789c00ba884d2
kienkauko <nguyentrungkiendhv@gmail.com>
2023-05-09 18:09:23 +0200

    add curl time

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 from time import sleep
-from main import *
+# from main import *
 from datetime import datetime
 from subprocess import call  
 import requests  
 import threading
 import re
 import string
-import multiservice_pods
+# import multiservice_pods
 
 pwd='1'
 cmd='kubectl apply -f /home/controller/knative-caculation/deployments/nginx-pod.yaml'
@@ -48,6 +48,25 @@ def is_pod_terminating():
     status = str(subprocess.run(['deployments/get_pod_status.sh', '-l'], stdout=subprocess.PIPE).stdout.decode('utf-8')).strip()
     return status == TERMINATING_STATUS 
 
+def curl_time():
+    # Define the command to run
+    # cmd = "./myscript.sh https://www.google.com"
+
+    # Run the command and capture its output
+    output = subprocess.check_output(['curltime.sh', 'google.com'])
+
+    # Extract the values you're interested in from the output
+    time_pretransfer = float(output.split(b"time_pretransfer: ")[1].split(b" ")[0])
+    time_redirect = float(output.split(b"time_redirect: ")[1].split(b" ")[0])
+
+    # Store the values in a dictionary
+    times = {"time_pretransfer": time_pretransfer, "time_redirect": time_redirect}
+
+    # Print the dictionary
+    print(times)
+
+
 if __name__=="__main__":
     # multiservice_pods.update_replicas(3, "pi4", "29061999/knative-video-detection-arm@sha256:47705b6d9561b0fe45fadf559802a8d500c32b069fd50ef0fc69e6859c34a9e3")
-    create_request_thread(8)
+    # create_request_thread(8)
+    curl_time()
